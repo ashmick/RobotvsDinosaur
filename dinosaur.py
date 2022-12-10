9b302865d181b3140461e5171fcd1232170ca292
ashmick <ashley.mcneill2014@gmail.com>
2022-12-09 21:44:44 -0500

    dinosaur.py class added

---
+++ dinosaur.py
@@ -0,0 +1,12 @@
+class Dinosaur:
+    pass
+
+    def __init__(self):
+        self.name=""
+        self.attack_power=int
+        self.health=int
+        pass
+    
+    def attack (self,name, attack_power):
+        self.attack= attack_power
+        pass
