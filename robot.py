0868bef897499c0499c9f466f6e0e377e6263665
ashmick <ashley.mcneill2014@gmail.com>
2022-12-09 21:39:23 -0500

    robot.py class added

---
+++ robot.py
@@ -0,0 +1,11 @@
+class Robot:
+    pass
+
+    def __init__(self) -> None:
+        self.name= ""
+        self.health=int
+        self.active_weapon="Weapon func"
+        pass
+    
+    def attack(dinosaur):
+        pass
